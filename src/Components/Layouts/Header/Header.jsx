b6ef36c2e9c6a1dd787f8470d9c984c743a94ce8
SENA <marianaocampomedina@gmail.com>
2025-03-06 14:46:51 -0500

    Proyecto-React-Portfolio

---
+++ src/Components/Layouts/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        
+    <h1 className="mt-2 text-4xl font-bold m-4 bg-yellow-600">MARIANA</h1>
+      
+  </header>
+  )
+}
